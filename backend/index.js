7d8992f2c535643780f2d63ee212a0c3d56ab078
sujal <pandeysujal258@gmail.com>
2023-12-10 18:27:29 +0545

    New project is added

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 5000;
+
+const static_path = path.join(__dirname, "../frontend");
+app.use(express.static(static_path));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my server!!");
+});
+
+app.listen(port, async () => {
+  try {
+    console.log(`http://localhost:${port}`);
+  } catch (error) {
+    console.log("Failed to listen server");
+  }
+});
