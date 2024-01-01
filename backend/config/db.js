76fecb30974ead56d7aad07e97febfe45c7179fd
sujal <pandeysujal258@gmail.com>
2024-01-01 14:29:36 +0545

    updated succesfully

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,25 @@
 const mysql = require("mysql2/promise");
+const { Sequlize } = require("sequelize");
 
-const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "srms",
+const sequelize = new Sequelize("srms", "root", "", {
+  host: "hostname",
+  dialect: "mysql",
 });
 
-const query = "select * from student_data";
+// const connection = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "srms",
+// });
 
-connection
-  .query(query)
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// const query = "select * from student_data";
+
+// connection
+//   .query(query)
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 const path = require("path");
-const port = 5000;
+
+const port = process.env.PORT || 5001;
 
 const static_path = path.join(__dirname, "../frontend");
 app.use(express.static(static_path));
